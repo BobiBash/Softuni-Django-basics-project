03877d47d82eb9fa3a191bda6ae369512d3901d7
hoppa7 <bobgon050@gmail.com>
2026-03-12 22:19:21 +0200

    fixed home.html bug where app didnt work with empty database

--- Wildlife_Expedition_Tracker/expeditions/models.py
+++ Wildlife_Expedition_Tracker/expeditions/models.py
@@ -6,31 +6,22 @@ from .validators import validate_text
 
 # Create your models here.
 class Expedition(models.Model):
-    title = models.CharField(
-        max_length=150
-    )
+    title = models.CharField(max_length=150)
     slug = models.SlugField(
         unique=True,
         blank=True,
     )
     target_species = models.ForeignKey(
-        "animals.Animal",
-        on_delete=models.CASCADE,
-        related_name="target_expeditions"
+        "animals.Animal", on_delete=models.CASCADE, related_name="target_expeditions"
     )
     expected_species = models.ManyToManyField(
-        "animals.Animal",
-            related_name="expected_expeditions",
-            blank=True
+        "animals.Animal", related_name="expected_expeditions", blank=True
     )
     description = models.TextField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    location = models.CharField(
-        max_length=150,
-        validators=[validate_text]
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=150, validators=[validate_text])
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -38,7 +29,6 @@ class Expedition(models.Model):
             self.slug = self._generate_unique_slug(base_slug)
 
         super().save(*args, **kwargs)
-
 
     def _generate_unique_slug(self, base_slug):
         slug = base_slug
@@ -52,4 +42,11 @@ class Expedition(models.Model):
 
     def get_absolute_url(self):
         from django.urls import reverse
+
         return reverse("expedition_detail", kwargs={"slug": self.slug})
+
+    def is_active(self):
+        from django.utils import timezone
+
+        today = timezone.now().date()
+        return self.start_date <= today <= self.end_date
